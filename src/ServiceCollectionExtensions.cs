afe55c3d4bfc4e15ae6e86e01e7df7cb28b9965f
ThomasWaite03 <thomas.waite03@gmail.com>
2025-02-23 16:08:32 -0700

    Added the option to only register specific services for dependency injection.

--- src/ServiceCollectionExtensions.cs
+++ src/ServiceCollectionExtensions.cs
@@ -3,6 +3,7 @@ using BlazorJS.Internals.Clipboard;
 using BlazorJS.Internals.Console;
 using BlazorJS.Internals.Storage;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace BlazorJS;
 
@@ -10,11 +11,37 @@ public static class ServiceCollectionExtensions
 {
     public static void AddBlazorJS(this IServiceCollection services)
     {
-        services.AddScoped<JSInvoker>();
+        services.AddJSConsole();
+        services.AddSessionStorage();
+        services.AddLocalStorage();
+        services.AddClipboard();
+    }
+
+    public static void AddJSConsole(this IServiceCollection services)
+    {
+        services.TryAddScoped<JSInvoker>();
 
         services.AddScoped<IJSConsole, JSConsole>();
+    }
+
+    public static void AddSessionStorage(this IServiceCollection services)
+    {
+        services.TryAddScoped<JSInvoker>();
+
         services.AddScoped<ISessionStorage, SessionStorage>();
+    }
+
+    public static void AddLocalStorage(this IServiceCollection services)
+    {
+        services.TryAddScoped<JSInvoker>();
+
         services.AddScoped<ILocalStorage, LocalStorage>();
+    }
+
+    public static void AddClipboard(this IServiceCollection services)
+    {
+        services.TryAddScoped<JSInvoker>();
+
         services.AddScoped<IClipboard, Clipboard>();
     }
 }
